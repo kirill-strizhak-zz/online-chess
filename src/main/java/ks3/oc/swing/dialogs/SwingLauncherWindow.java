0c1228c22de9f80d772a7726e32fccbcc9e3f30a
Kirill Strizhak <k.strizhak84@gmail.com>
2018-03-20 17:58:54 +0100

    #11 Fixed radio button selection issue and cleaned up launcher window a bit

--- src/main/java/ks3/oc/swing/dialogs/SwingLauncherWindow.java
+++ src/main/java/ks3/oc/swing/dialogs/SwingLauncherWindow.java
@@ -1,5 +1,6 @@
 package ks3.oc.swing.dialogs;
 
+import ks3.oc.Protocol;
 import ks3.oc.Starter;
 
 import javax.swing.JButton;
@@ -14,17 +15,18 @@ import java.awt.CheckboxGroup;
 import java.awt.Color;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ItemEvent;
 
 public class SwingLauncherWindow extends SwingDialogWindow {
 
+    private final Starter starter;
     private JTextField addr, port, name;
     private Checkbox cbServer, cbClient, cbWhite, cbBlack;
-    private String save = "localhost";
+    private String savedAddress = "localhost";
 
     public SwingLauncherWindow(Starter starter) {
         super(new JFrame("Start game"));
+        this.starter = starter;
         frame.setSize(frame.getInsets().left + frame.getInsets().right + 200,
                 frame.getInsets().top + frame.getInsets().bottom + 230);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -33,95 +35,101 @@ public class SwingLauncherWindow extends SwingDialogWindow {
         frame.setBackground(Color.black);
         frame.setResizable(false);
 
-        JPanel txtFields = new JPanel();
+        frame.getContentPane().add("North", createInputPanel());
+        frame.getContentPane().add("West", createTypePanel());
+        frame.getContentPane().add("East", createColorPanel());
+        frame.getContentPane().add("South", createStartButton());
+    }
+
+    private JPanel createInputPanel() {
+        JPanel panel = new JPanel();
         JPanel nameFields = new JPanel();
         JPanel addrFields = new JPanel();
-        txtFields.setLayout(new BorderLayout(10, 10));
+        panel.setLayout(new BorderLayout(10, 10));
         nameFields.setLayout(new BorderLayout());
         addrFields.setLayout(new BorderLayout());
-        txtFields.setSize(300, 30);
+        panel.setSize(300, 30);
         addr = new JTextField("localhost");
         addr.setEditable(false);
         addr.setText(" - - - - - - - - - - - - - - - - ");
+        addr.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                savedAddress = addr.getText();
+            }
+        });
         port = new JTextField("12345");
         name = new JTextField("Player");
         JLabel n = new JLabel("Your name:");
         JLabel a = new JLabel("Address:");
         port.setColumns(5);
-        frame.getContentPane().add("North", txtFields);
-        txtFields.add("North", nameFields);
+        panel.add("North", nameFields);
         nameFields.add("North", n);
         nameFields.add("Center", name);
-        txtFields.add("South", addrFields);
+        panel.add("South", addrFields);
         addrFields.add("North", a);
         addrFields.add("East", port);
         addrFields.add("Center", addr);
+        return panel;
+    }
 
-        JPanel cbTPanel = new JPanel();
-        JPanel cbCPanel = new JPanel();
-        cbTPanel.setLayout(new BorderLayout());
-        cbCPanel.setLayout(new BorderLayout());
-        JLabel t = new JLabel("You are:");
-        JLabel c = new JLabel("Your color:");
-        CheckboxGroup cbTypeGroup = new CheckboxGroup();
-        CheckboxGroup cbColorGroup = new CheckboxGroup();
-        cbServer = new Checkbox("Server", cbTypeGroup, false);
+    private JPanel createTypePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add("North", new JLabel("You are:"));
+
+        CheckboxGroup typeGroup = new CheckboxGroup();
+        cbServer = new Checkbox("Server", typeGroup, false);
         cbServer.setState(true);
-        cbClient = new Checkbox("Client", cbTypeGroup, false);
-        cbWhite = new Checkbox("White", cbColorGroup, false);
+        cbServer.addItemListener((event) -> {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                addr.setEditable(false);
+                addr.setText(" - - - - - - - - - - - - - - - - ");
+                cbWhite.setEnabled(true);
+                cbBlack.setEnabled(true);
+            }
+        });
+        panel.add("Center", cbServer);
+
+        cbClient = new Checkbox("Client", typeGroup, false);
+        cbClient.addItemListener((event) -> {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                addr.setEditable(true);
+                addr.setText(savedAddress);
+                cbWhite.setEnabled(false);
+                cbBlack.setEnabled(false);
+            }
+        });
+        panel.add("South", cbClient);
+
+        return panel;
+    }
+
+    private JPanel createColorPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add("North", new JLabel("Your color:"));
+
+        CheckboxGroup colorGroup = new CheckboxGroup();
+        cbWhite = new Checkbox("White", colorGroup, false);
         cbWhite.setState(true);
-        cbBlack = new Checkbox("Black", cbColorGroup, false);
-        frame.getContentPane().add("West", cbTPanel);
-        frame.getContentPane().add("East", cbCPanel);
-        cbTPanel.add("North", t);
-        cbTPanel.add("Center", cbServer);
-        cbTPanel.add("South", cbClient);
-        cbCPanel.add("North", c);
-        cbCPanel.add("Center", cbWhite);
-        cbCPanel.add("South", cbBlack);
+        panel.add("Center", cbWhite);
 
-        addr.addFocusListener(new FocusAdapter() {
-            @Override
-            public void focusLost(FocusEvent e) {
-                save = addr.getText();
-            }
-        });
-        cbServer.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
+        cbBlack = new Checkbox("Black", colorGroup, false);
+        panel.add("South", cbBlack);
+        return panel;
+    }
 
-                if (cbServer.getState()) {
-                    addr.setEditable(false);
-                    addr.setText(" - - - - - - - - - - - - - - - - ");
-                    cbWhite.setEnabled(true);
-                    cbBlack.setEnabled(true);
-                }
-            }
-        });
-        cbClient.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (cbClient.getState()) {
-                    addr.setEditable(true);
-                    addr.setText(save);
-                    cbWhite.setEnabled(false);
-                    cbBlack.setEnabled(false);
-                }
-            }
-        });
-
+    private JButton createStartButton() {
         JButton start = new JButton("Start");
         start.addActionListener(event -> {
             if ((!addr.getText().equals("")) && (!port.getText().equals("")) && (!name.getText().equals(""))) {
                 frame.setVisible(false);
-                int type, color1;
-                if (cbServer.getState()) type = 0;
-                else type = 1;
-                if (cbWhite.getState()) color1 = 2;
-                else color1 = 0;
-                starter.begin(type, color1, addr.getText(), Integer.parseInt(port.getText()), name.getText());
+                int type = cbServer.getState() ? Protocol.SERVER : Protocol.CLIENT;
+                int color = cbWhite.getState() ? Protocol.WHITE : Protocol.BLACK;
+                starter.begin(type, color, addr.getText(), Integer.parseInt(port.getText()), name.getText());
             }
         });
-        frame.getContentPane().add("South", start);
+        return start;
     }
 }
